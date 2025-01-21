31425dcd623c1f7d2c21a1e736c87b50a95ac1e6
Jonathas <jonathass5678@gmail.com>
2025-01-21 18:39:55 -0300

    server_sventv

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ const guia = require('./routes/guia');
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsConfig));
+// app.use(cors(corsConfig));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
 app.engine('html', require('ejs').renderFile);
