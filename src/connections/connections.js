d7396dd9fc094b7f0015310161574b3af7e1636a
Jonathas <jonathass5678@gmail.com>
2025-03-16 11:28:23 -0300

    sventv_server

--- src/connections/connections.js
+++ src/connections/connections.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 function createSupabaseClient() {
-  const supabaseUrl = 'https://lhtnjwrrpdnruaqwpmvp.supabase.co';
+  const supabaseUrl = 'https://vyaowvshvysnfwhffapb.supabase.co';
   const supabaseKey = process.env.SUPABASE_KEY;
   return createClient(supabaseUrl, supabaseKey); 
 }
